79090c0ce4bd9be029f8d5f1719badaa5106b27a
fishbigger <toby.e.m.johnson@gmail.com>
2021-01-17 15:11:20 +0000

    Added text to demo scene

---
+++ Uncontrollable Fate/Assets/Scripts/TextController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextController : MonoBehaviour
+{
+    public TextMeshProUGUI textBox;
+
+    public string Text;
+
+    private IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coroutine = WriteText(Text, textBox);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator WriteText(string text, TextMeshProUGUI textBox)
+    {
+        string writtenText = "";
+        foreach (char c in text) {
+            yield return new WaitForSeconds(0.01f);
+
+            writtenText = writtenText + c;
+
+            textBox.text = writtenText;
+        }
+    }
+}
